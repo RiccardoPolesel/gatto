65ed8aee2ee1c1a4f190b676c016570cee4104b0
RiccardoPolesel <poleselriccardo@gmail.com>
2018-11-25 18:42:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <algorithm>
 #include <iostream>
 #include <random>
+#include <string>
 #include <ctime>
+#include <math.h>
 #if defined(_WIN32)
 #include <SDL.h>
 #include <SDL_ttf.h>
@@ -12,22 +14,16 @@
 #include <SDL2/SDL_image.h>
 #endif
 
+using namespace std;
+
 void startSDL() {
-	if(SDL_Init(SDL_INIT_VIDEO) < 0)
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 		std::cout << "Error while initializing SDL: " << SDL_GetError();
-	if(TTF_Init() == -1)
+	if (TTF_Init() == -1)
 		std::cout << "Error while initializing SDL_ttf: " << TTF_GetError();
 	IMG_Init(IMG_INIT_PNG);
 }
 
-Uint32 getPixelColor(SDL_Texture* texture, int X, int Y) {
-	int pitch, w, h;
-	void *pixels;
-	SDL_QueryTexture(texture, NULL, NULL, &w, &h);
-	SDL_LockTexture(texture, NULL, &pixels, &pitch);
-	Uint32 *upixels = (Uint32*) pixels;
-	return upixels[(Y * w) + X];
-}
 
 void writeText(SDL_Renderer* renderer, int x, int y, const std::string& text, TTF_Font* font) {
 	int w, h;
@@ -38,169 +34,193 @@ void writeText(SDL_Renderer* renderer, int x, int y, const std::string& text, TT
 	SDL_Rect dRect{ x, y, w, h };
 	SDL_RenderCopy(renderer, textTexture, NULL, &dRect);
 	SDL_FreeSurface(textSurface);
+	SDL_DestroyTexture(textTexture);
 }
 
 class GrattaEVinci {
-  int montepremi[12] = {1, 5, 10, 50, 100, 1000, 5000, 10000, 100000, 500000, 1000000, 2000000};
-  int vincenti[5];
-  int jolly;
-  int numeri[15][2];
+	int montepremi[12] = { 1, 5, 10, 50, 100, 1000, 5000, 10000, 100000, 500000, 1000000, 2000000 };
+	int vincenti[5];
+	int jolly;
+	int numeri[15][2];
 	bool scoperti[21];
 	SDL_Window* window;
 	SDL_Texture* texture;
-	SDL_Renderer* renderer;
-	TTF_Font* font;
+	
 
 	bool controlla(int x, int y, int d, int d2);
 public:
-  GrattaEVinci();
-  void gratta(int x, int y, int r);
-  int vincita();
-  void render();
+	TTF_Font* font;
+	SDL_Renderer* renderer;
+	GrattaEVinci();
+	void gratta(int x, int y, int r);
+	int vincita();
+	void render();
 	bool finito();
 };
 
 GrattaEVinci::GrattaEVinci()
-: window(NULL), font(NULL) {
+	: window(NULL), font(NULL) {
 	window = SDL_CreateWindow("Gratta e Vinci", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 400, 800, SDL_WINDOW_SHOWN);
-	if(window == NULL ) {
+	if (window == NULL) {
 		std::cout << "Error while creating SDL_Window: " << SDL_GetError();
 		return;
 	}
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	if(renderer == NULL) {
+	if (renderer == NULL) {
 		std::cout << "Error while creating SDL_Renderer: " << SDL_GetError();
 		return;
 	}
-	font = TTF_OpenFont("OpenSans-Regular.ttf", 12);
-	if(font == NULL) {
+	font = TTF_OpenFont("Sans.ttf", 15);
+	if (font == NULL) {
 		std::cout << "Error while loading TTF_Font: " << TTF_GetError();
 		return;
 	}
-	SDL_Surface* surface = SDL_LoadBMP("texture.bmp");
-	if(surface == NULL) {
+	SDL_Surface* surface = IMG_Load("texture.jpg");
+	if (surface == NULL) {
 		std::cout << "Error while loading texture: " << SDL_GetError();
 		return;
 	}
-	SDL_Surface* formattedSurface = SDL_ConvertSurfaceFormat( surface, SDL_PIXELFORMAT_RGBA8888, 0 );
+	
+
+	
+
+	SDL_Surface* formattedSurface = SDL_ConvertSurfaceFormat(surface, SDL_PIXELFORMAT_RGBA8888, 0);
 	texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, formattedSurface->w, formattedSurface->h);
 
 	int pitch;
 	void* pixels;
-	SDL_LockTexture( texture, NULL, &pixels, &pitch );
-	memcpy( pixels, formattedSurface->pixels, formattedSurface->pitch * formattedSurface->h );
-	SDL_UnlockTexture( texture );
+	SDL_LockTexture(texture, NULL, &pixels, &pitch);
+	memcpy(pixels, formattedSurface->pixels, formattedSurface->pitch * formattedSurface->h);
+	SDL_UnlockTexture(texture);
 	pixels = NULL;
 	SDL_FreeSurface(surface);
 	SDL_FreeSurface(formattedSurface);
 	SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
+	
 
-  srand(time(NULL));
-  for(int i = 0; i < 5; i++) {
-    vincenti[i] = rand()%100;
-  }
-	jolly = rand()%100;
-  for(int i = 0; i < 15; i++) {
-    numeri[i][0] = rand()%100;
-    numeri[i][1] = montepremi[rand()%12];
-  }
-	for(int i = 0; i < 21; i++) {
+	srand(time(NULL));
+	for (int i = 0; i < 5; i++) {
+		vincenti[i] = rand() % 100;
+	}
+	jolly = rand() % 100;
+	for (int i = 0; i < 15; i++) {
+		numeri[i][0] = rand() % 100;
+		numeri[i][1] = montepremi[rand() % 12];
+	}
+	for (int i = 0; i < 21; i++) {
 		scoperti[i] = false;
 	}
-  render();
+	render();
 }
 
 int GrattaEVinci::vincita() {
-  int vincita = 0;
-  for(int i = 0; i < 15; i++) {
-    if(std::find(vincenti, vincenti+5, numeri[i][0]) != vincenti+5) {
-      vincita += numeri[i][1];
-    }
-  }
-  return vincita;
+	int vincita = 0;
+	for (int i = 0; i < 15; i++) {
+		if (std::find(vincenti, vincenti + 5, numeri[i][0]) != vincenti + 5) {
+			vincita += numeri[i][1];
+		}
+	}
+	return vincita;
 }
 
 void GrattaEVinci::gratta(int x, int y, int r) {
-	int pitch, w, h;
+	int pitch=0, w, h;
 	void *pixels;
 	SDL_QueryTexture(texture, NULL, NULL, &w, &h);
 	SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
-	SDL_LockTexture(texture, NULL, &pixels, &pitch);
-	Uint32 *upixels = (Uint32*) pixels;
-	SDL_PixelFormat* mappingFormat = SDL_AllocFormat( SDL_PIXELFORMAT_RGBA8888 );
+	SDL_LockTexture(texture, NULL, (void**)&pixels, &pitch);
+	Uint32 *upixels = (Uint32*)pixels;
+	SDL_PixelFormat* mappingFormat = SDL_AllocFormat(SDL_PIXELFORMAT_RGBA8888);
 	Uint32 transparent = SDL_MapRGBA(mappingFormat, 0x00, 0x00, 0x00, 0x00);
 
-	for(int i = 0; i < r; i++) {
-		for(int xp = 0; xp <= i; xp++) {
-			int yp = sqrt(pow(i, 2) - pow(xp, 2));
-			upixels[(yp+y)*(pitch / sizeof(unsigned int)) + xp + x] = transparent;
-			upixels[(-yp+y)*(pitch / sizeof(unsigned int)) + xp + x] = transparent;
-			upixels[(yp+y)*(pitch / sizeof(unsigned int)) - xp + x] = transparent;
-			upixels[(-yp+y)*(pitch / sizeof(unsigned int)) - xp + x] = transparent;
+	for(int a=0;a<360;a+=2){
+		for (int i = 0; i < r; i++) {
+			int X = x + cos(a*3.14/180)*i;
+			int Y = y + sin(a*3.14/180)*i;
+			
+			upixels[(Y)*pitch/sizeof(unsigned int) + X] = transparent;
 		}
 	}
+	
 
-	memcpy(pixels, upixels, pitch * h);
 	SDL_UnlockTexture(texture);
+	SDL_FreeFormat(mappingFormat);
 }
 
 void GrattaEVinci::render() {
-	SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+	SDL_SetRenderDrawColor(renderer, 128, 128, 128, 0xFF);
 	SDL_RenderClear(renderer);
+	SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
+	
 
-	for(int i = 0; i < 5; i++) {
-		writeText(renderer, (80 + i*120) / 2, 900/2, std::to_string(vincenti[i]), font);
+	for (int i = 0; i < 5; i++) {
+		writeText(renderer, (80 + i * 120) / 2, 900 / 2, std::to_string(vincenti[i]), font);
 	}
-	writeText(renderer, 680/2, 900/2, std::to_string(jolly), font);
-	for(int i = 0; i < 5; i++) {
-    for(int l = 0; l < 3; l++) {
-      writeText(renderer, (100 + i*(140)) / 2, (1100 + 135*l) / 2, std::to_string(numeri[i*3+l][0]), font);
-			writeText(renderer, (60 + i*(140)) / 2, (1160 + 135*l) / 2, std::to_string(numeri[i*3+l][1]) + "€", font);
-  	}
-  }
+	writeText(renderer, 680 / 2, 900 / 2, std::to_string(jolly), font);
+	for (int i = 0; i < 5; i++) {
+		for (int l = 0; l < 3; l++) {
+			writeText(renderer, (100 + i*(140)) / 2, (1100 + 135 * l) / 2, std::to_string(numeri[i * 3 + l][0]), font);
+			writeText(renderer, (60 + i*(140)) / 2, (1160 + 135 * l) / 2, std::to_string(numeri[i * 3 + l][1]) + "€", font);
+		}
+	}
 
 	SDL_RenderCopy(renderer, texture, NULL, NULL);
 	SDL_RenderPresent(renderer);
 }
 
 bool GrattaEVinci::controlla(int x, int y, int d, int d2) {
-  for(int i = 0; i < d; i += 5) {
-    for(int j = 0; j < d2; j += 5) {
-      if(getPixelColor(texture,x+i,y+j) != 0) {
-    		return false;
-      }
-  	}
-  }
-  return true;
+
+
+	int pitch, w, h;
+	void *pixels;
+	SDL_QueryTexture(texture, NULL, NULL, &w, &h);
+	SDL_LockTexture(texture, NULL, &pixels, &pitch);
+	Uint32 *upixels = (Uint32*)pixels;
+	SDL_UnlockTexture(texture);
+	for (int i = 0; i < d; i += 15) {
+		for (int j = 0; j < d2; j += 15) {
+			if (upixels[((y+j) * w) + x+1] != 0) {
+				return false;
+			}
+		}
+	}
+	return true;
 }
 
 bool GrattaEVinci::finito() {
-  for(int i = 0; i < 6; i++) {
-    if(!scoperti[i]) {
-      scoperti[i] = controlla(50 + i*115, 880, 90, 90);
-      if(!scoperti[i]) return false;
-    }
-  }
-  for(int i = 0; i < 5; i++) {
-    for(int l = 0; l < 3; l++) {
-      if(!scoperti[5+i*3+l]) {
-        scoperti[5+i*3+l] = controlla(70 + i*(140), 1070 + 135*l, 90, 130);
-        if(!scoperti[5+i*3+l]) return false;
-	 		}
-  	}
-  }
-  return true;
+	for (int i = 0; i < 6; i++) {
+		if (!scoperti[i]) {
+			scoperti[i] = controlla(50 + i * 115, 880, 90, 90);
+			if (!scoperti[i]) return false;
+		}
+	}
+	for (int i = 0; i < 5; i++) {
+		for (int l = 0; l < 3; l++) {
+			if (!scoperti[5 + i * 3 + l]) {
+				scoperti[5 + i * 3 + l] = controlla(70 + i*(140), 1070 + 135 * l, 90, 130);
+				if (!scoperti[5 + i * 3 + l]) return false;
+			}
+		}
+	}
+	return true;
 }
 
-int main() {
+int main(int argc, char* args[]) {
 	startSDL();
-
+	bool button_down = false;
 	GrattaEVinci a;
 	bool esci = false;
 	SDL_Event u;
 	while (!esci)
 	{
 		a.render();
+		if (button_down) {
+			int x, y;
+			SDL_GetMouseState(&x, &y);
+			a.gratta(x*2, y*2, 60);
+		}
+
+
 		while (SDL_PollEvent(&u) != 0)
 		{
 			switch (u.type) {
@@ -208,9 +228,25 @@ int main() {
 				esci = true;
 				break;
 			case SDL_MOUSEBUTTONDOWN:
-				a.gratta(u.button.x, u.button.y, 10);
-				a.render();
+				button_down = true;
+				break;
+			case SDL_MOUSEBUTTONUP:
+				button_down = false;
+				break;
 			}
 		}
+
+		if (a.finito()) {
+			esci = true;
+		}
 	}
+
+	SDL_SetRenderDrawColor(a.renderer, 128, 128, 128, 0xFF);
+	SDL_RenderClear(a.renderer);
+	SDL_SetRenderDrawColor(a.renderer, 0x00, 0x00, 0x00, 0xFF);
+	writeText(a.renderer, 10, 10, std::to_string(a.vincita()), a.font);
+	SDL_RenderPresent(a.renderer);
+
+	SDL_Delay(5000);
+	return 0;
 }
